b454ee909b5020eff5bddf4a5fcb3cf97f981460
Maxim Tsatsura <maxim.tsatsura@gmail.com>
2022-12-19 20:23:21 +0300

    feat: basket-line

---
+++ local/templates/lassie/components/bitrix/sale.basket.basket.line/.default/template.php
@@ -0,0 +1,18 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+  die();
+}
+/**
+ * @global string $componentPath
+ * @global string $templateName
+ * @var CBitrixComponentTemplate $this
+ */
+
+?>
+
+<div class="header__basket">
+    <div class="text">В вашей корзине</div>
+    <a href="<?= $arParams["PATH_TO_BASKET"] ?>" class="link"><?= $arResult["NUM_PRODUCTS"] ?> товара
+        на <?= $arResult["TOTAL_PRICE_RAW"] ?> руб.</a>
+</div>
+
+
--- local/templates/lassie/header.php
+++ local/templates/lassie/header.php
@@ -98,7 +98,7 @@ $curPage = $APPLICATION->GetCurPage(true);
         <div class="container header__container header__container_middle">
             <div class="header__col header__col_logo">
                 <a href="javascript:void(0);" class="header__logo logo">
-                    <img src="assets/images/logo.png" class="logo__img" alt="">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/logo.png" class="logo__img" alt="">
                 </a>
             </div>
             <div class="header__contacts"><span class="header__icon icon-comment"></span>
@@ -112,7 +112,15 @@ $curPage = $APPLICATION->GetCurPage(true);
                           ),
                           false
                         );?></a>
-                        <div class="contacts__info">с 9:00 до 24:00 ежедневно</div>
+                        <div class="contacts__info"><?$APPLICATION->IncludeComponent(
+                            "bitrix:main.include",
+                            "",
+                            array(
+                              "AREA_FILE_SHOW" => "file",
+                              "PATH" => SITE_DIR."include/schedule1.php"
+                            ),
+                            false
+                          );?></div>
                     </div>
                 </div>
                 <div class="header__col header__col_contacts">
@@ -126,16 +134,44 @@ $curPage = $APPLICATION->GetCurPage(true);
                           ),
                           false
                         );?></a>
-                        <div class="contacts__info">24 часа 7 дней в неделю</div>
+                        <div class="contacts__info"><?$APPLICATION->IncludeComponent(
+                            "bitrix:main.include",
+                            "",
+                            array(
+                              "AREA_FILE_SHOW" => "file",
+                              "PATH" => SITE_DIR."include/schedule2.php"
+                            ),
+                            false
+                          );?></div>
                     </div>
                 </div>
                 <div class="header__col header__col_contacts"><a href="javascript:void(0);" class="link">Контактная информация</a>
                 </div>
             </div>
             <div class="header__col header__col_basket"><span class="header__icon icon-bag"></span>
-                <div class="header__basket">
-                    <div class="text">В вашей корзине</div><a href="javascript:void(0);" class="link">4 товара на 25 196 руб.</a>
-                </div>
+              <?$APPLICATION->IncludeComponent(
+	"bitrix:sale.basket.basket.line", 
+	".default", 
+	array(
+		"PATH_TO_BASKET" => SITE_DIR."personal/cart/",
+		"PATH_TO_PERSONAL" => SITE_DIR."personal/",
+		"SHOW_PERSONAL_LINK" => "N",
+		"SHOW_NUM_PRODUCTS" => "Y",
+		"SHOW_TOTAL_PRICE" => "Y",
+		"SHOW_PRODUCTS" => "N",
+		"POSITION_FIXED" => "N",
+		"SHOW_AUTHOR" => "Y",
+		"PATH_TO_REGISTER" => SITE_DIR."login/",
+		"PATH_TO_PROFILE" => SITE_DIR."personal/",
+		"COMPONENT_TEMPLATE" => ".default",
+		"PATH_TO_ORDER" => SITE_DIR."personal/order/make/",
+		"SHOW_EMPTY_VALUES" => "Y",
+		"PATH_TO_AUTHORIZE" => "",
+		"SHOW_REGISTRATION" => "Y",
+		"HIDE_ON_BASKET_PAGES" => "Y"
+	),
+	false
+);?>
             </div>
         </div>
     </div>
